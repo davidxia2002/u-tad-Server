a14b5688ca7e24b523ee84e695802016a0eeb2f7
rpmaya <35469068+rpmaya@users.noreply.github.com>
2023-03-02 18:59:55 +0100

    JWT

--- MVC/controllers/auth.js
+++ MVC/controllers/auth.js
@@ -1,22 +1,42 @@
 const { matchedData } = require("express-validator")
 const { tokenSign } = require("../utils/handleJwt")
 const { encrypt, compare } = require("../utils/handlePassword")
+const {handleHttpError} = require("../utils/handleError")
 const {usersModel} = require("../models")
 
-const loginController = async (req, res) => {
-    req = matchedData(req)
-    const password = await encrypt(req.password)
-    const body = {...req, password} // Con "..." duplicamos el objeto y le añadimos o sobreescribimos una propiedad
-    const dataUser = await usersModel.create(body)
-    //Si no queremos que se devuelva el hash con "findOne", en el modelo de users ponemos select: false en el campo password
-    //Además, en este caso con "create", debemos setear la propiedad tal que:  
-    dataUser.set('password', undefined, { strict: false })
+/**
+ * Encargado de registrar un nuevo usuario
+ * @param {*} req 
+ * @param {*} res 
+ */
+const registerCtrl = async (req, res) => {
+    try {
+        req = matchedData(req)
+        const password = await encrypt(req.password)
+        const body = {...req, password} // Con "..." duplicamos el objeto y le añadimos o sobreescribimos una propiedad
+        const dataUser = await usersModel.create(body)
+        //Si no queremos que se devuelva el hash con "findOne", en el modelo de users ponemos select: false en el campo password
+        //Además, en este caso con "create", debemos setear la propiedad tal que:  
+        dataUser.set('password', undefined, { strict: false })
 
-    const data = {
-        token: await tokenSign(dataUser),
-        user: dataUser
+        const data = {
+            token: await tokenSign(dataUser),
+            user: dataUser
+        }
+        res.send(data)  
+    }catch(err) {
+        handleHttpError(res, "ERROR_REGISTER_USER")
     }
-    res.send(data)  
 }
 
-module.exports = { loginController }
+
+/**
+ * Encargado de hacer login del usuario
+ * @param {*} req 
+ * @param {*} res 
+ */
+const loginCtrl = async (req, res) => {
+    //TODO
+}
+
+module.exports = { registerCtrl, loginCtrl }
--- MVC/routes/auth.js
+++ MVC/routes/auth.js
@@ -1,15 +1,13 @@
 const express = require("express")
-const { loginController } = require("../controllers/auth")
+const { registerCtrl, loginCtrl } = require("../controllers/auth")
 const {validatorRegister, validatorLogin} = require("../validators/auth")
 const router = express.Router()
 
 
 //TODO Crear petición en index.http: http://localhost:3000/api/auth/register
-router.post("/register", validatorRegister, loginController)
+router.post("/register", validatorRegister, registerCtrl)
 
 //TODO Crear petición en index.http: http://localhost:3000/api/auth/login
-router.post("/login", (req, res) => {
-
-}) 
+router.post("/login", validatorLogin, loginCtrl) 
 
 module.exports = router
